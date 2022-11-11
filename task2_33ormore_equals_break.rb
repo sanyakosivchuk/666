816737a2e371a1badb64b0785cb99b7c5ab6ff3f
sanyakosivchuk <kosivcuks@gmail.com>
2022-11-11 19:50:43 +0000

    33breaker_fixed

--- task2_33ormore_equals_break.rb
+++ task2_33ormore_equals_break.rb
@@ -1,11 +1,9 @@
-values = [rand(1..9) + rand(1..9) + rand(1..9) + rand(1..9) + rand(1..9)]
-sum = 0
-values.each do |i|
-    sum += i
-    if sum <33 
-        puts sum
-        next
-    elsif sum >=33
-        break
+loop do 
+    values = [rand(1..9), rand(1..9), rand(1..9), rand(1..9),rand(1..9)]
+    sum = 0
+    values.each do |i|
+      sum += i
     end
-end
+    puts sum
+    break if sum == 33
+  end
