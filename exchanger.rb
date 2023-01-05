9a66dd1f1d6799d25950d8bf0eeda394cf62c43d
sanyakosivchuk <kosivcuks@gmail.com>
2023-01-05 21:38:11 +0000

    finalexchanger

--- exchanger.rb
+++ exchanger.rb
@@ -1,6 +1,6 @@
 hash = {'usd' => 36.77, 'eur' => 38.68}
 puts 'vvedit usd or eur'
-currency = gets.split('')
+currency = gets[0..2]
 puts 'vvedit kst valuty'
 amount = gets.to_f
-puts "result = #{amount*hash.fetch(currency[0]+currency[1]+currency[2])}"
+puts "result = #{amount*hash.fetch(currency)}"
